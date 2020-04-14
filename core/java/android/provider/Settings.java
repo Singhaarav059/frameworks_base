b5df58bd624376b80580635e503bae32e69807b4
Anushek Prasal <anushekprasal@gmail.com>
2020-04-14 23:41:01 +0530

    base: Swipe up on keyguard to use Face Unlock [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -10699,6 +10699,12 @@ public final class Settings {
         public static final Validator TETHERING_ALLOW_VPN_UPSTREAMS_VALIDATOR = BOOLEAN_VALIDATOR;
 
         /**
+         * Whether face unlock is allowed only on security view.
+         * @hide
+         */
+        public static final String FACE_UNLOCK_ALWAYS_REQUIRE_SWIPE = "face_unlock_always_require_swipe";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
